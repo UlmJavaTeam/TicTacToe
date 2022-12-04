dbab224cab044da980a2d23dfeb8f86816c56071
Kenan <Kenan@informatik.de>
2022-12-04 22:03:15 +0100

    deneme yaptım, umarım olmuştur. kenan

--- src/main/java/Ozan/TicTocToe.java
+++ src/main/java/Ozan/TicTocToe.java
@@ -2,6 +2,8 @@ package Ozan;
 
 public class TicTocToe {
     public static void main(String[] args) {
+
         System.out.println("Game will be started...");
+        System.out.println("Game has been already started...");
     }
 }
